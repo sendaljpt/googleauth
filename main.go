2e0552b7a74299ee6f8eabcc722edd27f2a18b62
Fajrin Imam Arif <fajrin.a@globalservice.co.id>
2023-11-16 07:53:32 +0700

    init google auth

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"log"
+
+	"github.com/gorilla/pat"
+	"github.com/gorilla/sessions"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+)
+
+func main() {
+	key := "Secret-session-key"
+	maxAge := 864 * 30
+	isProd := false
+
+	store := sessions.NewCookieStore([]byte(key))
+	store.MaxAge(maxAge)
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true
+	store.Options.Secure = isProd
+
+	gothic.Store = store
+
+	goth.UseProviders(
+		google.New("our-google-client-id", "our-google-client-secret", "http://localhost:3000/auth/google/callback", "email", "profile"),
+	)
+
+	p := pat.New()
+	p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
+
+		user, err := gothic.CompleteUserAuth(res, req)
+
+		if err != nil {
+			fmt.Println(res, err)
+			return
+		}
+
+		t, _ := template.ParseFiles("templates/success.html")
+		t.Execute(res, user)
+
+	})
+
+	p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		gothic.BeginAuthHandler(res, req)
+	})
+
+	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		t, _ := template.ParseFiles("templates/index.html")
+		t.Execute(res, false)
+	})
+
+	log.Println("listening on localhost:3000")
+	log.Fatal(http.ListenAndServe(":3000", p))
+}
